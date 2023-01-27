dc001bcc22d6113e4c25c5c78c6a48aee7bac2d4
lee rohrer <lrohrer@hunter.com>
2023-01-27 15:09:18 -0600

    initial commit

---
+++ server/.history/index_20230127145402.js
@@ -0,0 +1,29 @@
+'use strict'
+const express = require('express')
+
+// Create the express app
+const app = express()
+
+// Routes and middleware
+// app.use(/* ... */)
+// app.get(/* ... */)
+
+// Error handlers
+// app.use(function fourOhFourHandler (req, res) {
+//   res.status(404).send()
+// })
+// app.use(function fiveHundredHandler (err, req, res, next) {
+//   console.error(err)
+//   res.status(500).send()
+// })
+app.get('/*', (req, res) => {
+  res.json({ message: 'Success'})
+})
+// Start server
+app.listen(1234, function (err) {
+  if (err) {
+    return console.error(err)
+  }
+
+  console.log('Started at http://localhost:1234')
+})
